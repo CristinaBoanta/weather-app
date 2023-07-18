659430406e00009956e12cc7d19087f98c2b9f23
cristina_b <cristina_boanta@proton.me>
2023-07-19 00:46:14 +0300

    initial commit and project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    // fetch('http://api.weatherapi.com/v1/current.json?key=05f05a14a0ca402984b03203233001&q=Oradea')
+    // .then((response) => {
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+    //   return response.json();
+    // })
+    // .then((data) => console.log(data))
+
+    fetch(
+      "http://api.weatherapi.com/v1/forecast.json?key=05f05a14a0ca402984b03203233001&days=3&q=Oradea"
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      });
+  }, []);
+  
+
+  const getDayOfTheWeek = (dateString) => {
+    const dateObj = new Date(dateString);
+    const dayOfWeekText = dateObj.toLocaleDateString('en-US', { weekday: 'long' });
+    return dayOfWeekText;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-full">
+      <div className="bg-gray-100 h-full">
+        <div className="container mx-auto py-8">
+          <h1 className="text-3xl font-bold text-center mb-6">
+            Weather App Dashboard
+          </h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {weatherData &&
+              weatherData.forecast.forecastday.map((forecastItem) => {
+                return (
+                    <div className="bg-white p-4 rounded-lg shadow-md">
+                      <h2 className="text-xl font-bold mb-2">{getDayOfTheWeek(forecastItem.date)}</h2>
+                    </div>
+                );
+              })}
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
